39ca2f0af2091e0747e1fb30c50bc1bcfae81a07
HoussemHadded <houssem.hadded@sesame.com.tn>
2025-11-27 23:08:26 +0100

    modification style

--- register.php
+++ register.php
@@ -19,7 +19,7 @@ if (isLoggedIn()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription - Restaurant Les Jomox</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
